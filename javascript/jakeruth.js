aec15704e5659cac893d0d3f5ae9092bfe187add
Jake Ruth <jruth@albany.edu>
2015-04-25 16:56:25 -0400

    Removed the developer buttons :''(, getting ready for official debut

--- javascript/jakeruth.js
+++ javascript/jakeruth.js
@@ -1,32 +1,4 @@
 $(function () {
-	var i = 1;
-	var background;
-	
-	$('#1').on('click', function () {
-		if ((background === undefined) || (background === null)) {
-		background = setInterval(function () {console.log(background);
-			if (i > 21) {
-				i = 1;
-			}
-			
-			$('.background').attr('src', 'images/Polygons/' + (i++) + '.jpg');
-		}, 1800);
-		}
-	});
-	$('#2').on('click', function () {
-		clearInterval(background);
-		background = null;
-	});
-	$('#3').on('click', function () {
-		$('.home1').hide();
-		$('.home2').show();
-	});
-	$('#4').on('click', function () {
-		$('.home2').hide();
-		$('.home1').show();
-	});
-	
-	
 	$('.resume-button').on('click', openResumeViewer);
 	$('#back-button').on('click', closeResumeViewer);
 	$('#back-arrow').on('click', closeResumeViewer);
